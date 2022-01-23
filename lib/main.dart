aa542bfad23ea85d4568cb80e24e2b4ffbe1a65d
CarlosGM <carlos.garciamoncada@gmail.com>
2022-01-22 18:23:13 -0600

    Finished welcome screen

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final themedark = ThemeData.light();
+    // final theme = ThemeData.light();
     return MaterialApp(
-      title: 'Calendario FEMCOBI 2022',
-      theme: themedark,
-      //primarySwatch: Colors.red,
+      title: 'Calendario 2022',
+      theme: ThemeData(primarySwatch: Colors.green),
       home: const FirstRoute(),
     );
   }
@@ -27,46 +26,61 @@ class FirstRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   title: const Text('Calendario FEMCOBI 2022'),
-        // ),
-        body: Center(
-      child: Column(children: [
-        Stack(children: [
-          Image.asset(
-            'assets/bee.jpg',
-            width: double.infinity,
-            // fit: BoxFit.cover,
-          ),
-          Center(
-            child: Text('Calendario FEMCOBI 2022'),
-          ),
-        ]),
-        Container(
-            padding: EdgeInsets.all(10.0),
-            child: Image.asset(
-              'assets/logos/logo_FEMCOBI.png',
-              // width: 200,
-            )),
-        ElevatedButton(
-          child: const Text('Calendario'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const MyHomePage()),
-            );
-          },
+        appBar: AppBar(
+          title: const Text('Calendario FEMCOBI 2022'),
         ),
-        ElevatedButton(
-          child: const Text('Agradecimientos'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SecondRoute()),
-            );
-          },
-        ),
-      ]),
-    ));
+        body: Stack(
+          children: [
+            ShaderMask(
+              shaderCallback: (rect) {
+                return const LinearGradient(
+                  begin: Alignment.center,
+                  end: Alignment.topCenter,
+                  colors: [Colors.black, Colors.transparent],
+                ).createShader(Rect.fromLTRB(0, 0, rect.width, rect.height));
+              },
+              blendMode: BlendMode.dstIn,
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/bee.jpg'), fit: BoxFit.cover),
+                ),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset('assets/logos/logo_FEMCOBI.png'),
+                Center(
+                  child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          child: const Text('Calendario'),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const MyHomePage()),
+                            );
+                          },
+                        ),
+                        ElevatedButton(
+                          child: const Text('Agradecimientos'),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SecondRoute()),
+                            );
+                          },
+                        ),
+                      ]),
+                ),
+              ],
+            )
+          ],
+        ));
   }
 }
